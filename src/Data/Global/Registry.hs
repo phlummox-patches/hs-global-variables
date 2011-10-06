21226ba1a8d5ea6e732022e28aeb8a36f9e69f4d
Jean-Marie Gaillourdet <jmg@gaillourdet.net>
2011-10-06 17:55:16 +0200

    provisional fix of the concurrent initialization issue and refactoring to sole registry without store

--- src/Data/Global/Registry.hs
+++ src/Data/Global/Registry.hs
@@ -2,6 +2,7 @@ module Data.Global.Registry where
 
 import Control.Applicative ((<$>))
 import Control.Concurrent.MVar
+import Control.Exception.Base
 import Data.Dynamic
 import Data.Map as M
 
@@ -11,7 +12,7 @@ type Registry cell = MVar (Map String cell)
 
 
 setupRegistryIO :: IO (Registry cell)
-setupRegistryIO = newMVar $ M.empty
+setupRegistryIO = (newMVar $ M.empty)
 
 
 
@@ -20,7 +21,14 @@ lookupIO :: (ref Dynamic -> cell)            -- GVar
          -> (Registry cell) 
          -> String 
          -> IO cell
-lookupIO wrapper maker registry name = modifyMVar registry lkup
+lookupIO wrapper maker registry name = -- modifyMVar registry lkup
+ do { old <- takeMVar registry
+    ; (new, res) <- lkup old
+    ; evaluate res
+    ; evaluate new
+    ; putMVar registry new
+    ; return res
+    }
   where
     lkup reg = case M.lookup name reg of
         Just k' -> return (reg, k')
--- test-src/TestMain.hs
+++ test-src/TestMain.hs
@@ -4,10 +4,17 @@ import Test.Framework (defaultMain)
 
 import Data.Global.IORef.Test as I
 import Data.Global.TVar.Test as T
+import Data.Global.Registry.Test as R
 
+import Data.Global.IORef
 
 main ::  IO ()
-main = defaultMain
-    [ I.tests
-    , T.tests
-    ]
+main = 
+  do
+    -- let x = declare "foo"
+    -- print $ x == x
+    defaultMain
+        [ -- R.tests
+         I.tests
+    --    , T.tests
+        ]
